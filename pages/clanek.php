2f1ab90dfd4d4982816e02205eb6b869f014c234
Radim Nyč <43829570+Rychleji@users.noreply.github.com>
2019-01-18 11:10:22 +0100

    načítání článku z DB

--- pages/clanek.php
+++ pages/clanek.php
@@ -1,6 +1,17 @@
 <!-- CSS rules for styling the element inside the editor such as p, h1, h2, etc. -->
 <link href="../css/froala_style.min.css" rel="stylesheet" type="text/css" />
 
+<?php
+$conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $conn->prepare("SELECT * FROM clanky WHERE id_clanku = :idCl");
+
+$stmt->bindParam(':idCl', $_GET["clanek"]);
+$stmt->execute();
+$clanek = $stmt->fetch();
+?>
+<h3><?php echo $clanek['nazev']; ?></h3>
 <div class="fr-view">
-    Here comes the HTML edited with the Froala rich text editor.
+    <?php echo $clanek['text_clanku']; ?>
 </div>
